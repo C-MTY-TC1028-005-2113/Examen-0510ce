23552575a1ac33fd953f266cd256bed1ec9d6381
roque63 <roque@itesm.mx>
2021-09-08 20:44:00 -0500

    Update exercise.py

--- assignments/01CuentaNumeros/src/exercise.py
+++ assignments/01CuentaNumeros/src/exercise.py
@@ -1,21 +1,10 @@
 def sistema_ecuaciones( ):
-    a = int(input( 'a = ') )
-    b = int(input( 'b = ') )
-    c = int(input( 'c = ') )
-    d = int(input( 'd = ') )
-    e = int(input( 'e = ') )
-    f = int(input( 'f = ') )
-
-    y = (a * f - c * d) / (a * e - b * d)
-    x = (c * e - b * f) / (a * e - b * d)
-
-    print('x =', x)
-    print('y =', y)
+    pass
 
 
 def main():
-  #escribe tu código abajo de esta línea
-  sistema_ecuaciones( )
+  # llama a la función sistema_ecuaciones
+  pass
 
 
 if __name__=='__main__':
