1deeb87e31d5a17626c9e359616aba2a2baebb97
roque63 <roque@itesm.mx>
2021-09-08 20:44:33 -0500

    Update exercise.py

--- assignments/02SumaNumeros/src/exercise.py
+++ assignments/02SumaNumeros/src/exercise.py
@@ -1,23 +1,6 @@
 # función contar_ diagnosticos
 def contar_diagnosticos(n):
-    casos_delta = 0
-    casos_covid19 = 0
-    sanos = 0
-    for vcc in range(n):
-        diag = input("Ingresa diagnostico: ")
-        if diag in ['D', 'd']:
-            casos_delta = casos_delta  + 1
-        elif diag in ['C', 'c']:
-            casos_covid19 = casos_covid19 + 1
-        elif diag in ['S','s']:
-            sanos = sanos + 1
-
-    if casos_delta > 0:
-        print("Casos Delta =", casos_delta)
-    if casos_covid19 > 0:
-        print("Casos Covid-19 =", casos_covid19)
-    if sanos > 0:
-        print("Casos Sanos =", sanos)
+    pass
 
 
 def main( ):
@@ -25,7 +8,7 @@ def main( ):
     n = int(input("Ingresa el total de diagnosticos: "))
 
     # Llamar a la función contar_diagnosticos
-    contar_diagnosticos(n)
+    pass
 
 if __name__=='__main__':
     main()
