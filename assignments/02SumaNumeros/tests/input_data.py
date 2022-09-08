4e7075f86f7dedbc1c1fe9e2ce6add6f2d347015
roque63 <roque@itesm.mx>
2022-09-07 21:02:47 -0500

    Update input_data.py

--- assignments/02SumaNumeros/tests/input_data.py
+++ assignments/02SumaNumeros/tests/input_data.py
@@ -6,32 +6,83 @@
 # To test another case, add another tuple
 
 input_values = [
-    # Test case 1
-    (
-        ["6","c","d","s","c","d","s"],
-        ["Ingresa el total de diagnosticos: ",
-        "Ingresa diagnostico: ","Ingresa diagnostico: ","Ingresa diagnostico: ","Ingresa diagnostico: ","Ingresa diagnostico: ","Ingresa diagnostico: ",
-        "Casos Delta = 2", "Casos Covid-19 = 2", "Casos Sanos = 2"],
-        "Revisa que hayas inicializado los contadores antes del ciclo, revisa si verificaste si se ingresaron valores de ese diganostico usando condicionales"
+      (
+        ["U","F","10"],
+        ["Tipo de cursos: ", "Tipo de alumno: ", "Cantidad de cursos: ", 
+         "Colegiatura $65000 DLS", "Descuento $7800 DLS", "Total a pagar $57200 DLS"],
+        "Revisa que ingresastelos tipos de datos correctos, revisa que los datos de salida sean enteros\
+        revisa los espacios"
     ),
     (
-        ["8","C","D","S","c","D","s","s","s"],
-        ["Ingresa el total de diagnosticos: ",
-        "Ingresa diagnostico: ","Ingresa diagnostico: ","Ingresa diagnostico: ","Ingresa diagnostico: ",
-        "Ingresa diagnostico: ","Ingresa diagnostico: ",
-        "Ingresa diagnostico: ","Ingresa diagnostico: ","Casos Delta = 2", "Casos Covid-19 = 2", "Casos Sanos = 4"],
-        "Revisa que hayas inicializado los contadores antes del ciclo, revisa si verificaste si se ingresaron valores de ese diganostico usando condicionales"
+        ["B","F","10"],
+        ["Tipo de cursos: ", "Tipo de alumno: ", "Cantidad de cursos: ",
+         "Colegiatura $89000 DLS", "Descuento $24030 DLS", "Total a pagar $64970 DLS"],
+        "Revisa que ingresastelos tipos de datos correctos, revisa que los datos de salida sean enteros\
+        revisa los espacios"
     ),
     (
-        ["0"],
-        ["Ingresa el total de diagnosticos: "],
-        "Revisa que hayas inicializado los contadores antes del ciclo, revisa si verificaste si se ingresaron valores de ese diganostico usando condicionales"
-
+        ["I","F","10"],
+        ["Tipo de cursos: ", "Tipo de alumno: ", "Cantidad de cursos: ",
+        "Colegiatura $305500 DLS", "Descuento $82485 DLS", "Total a pagar $223015 DLS"],
+         "Revisa que ingresastelos tipos de datos correctos, revisa que los datos de salida sean enteros\
+        revisa los espacios"
     ),
+         
+         
+   (
+        ["U","L","10"],
+        ["Tipo de cursos: ", "Tipo de alumno: ", "Cantidad de cursos: ", 
+         "Colegiatura $65000 DLS", "Descuento $19500 DLS","Total a pagar $45500 DLS"],
+        "Revisa que ingresastelos tipos de datos correctos, revisa que los datos de salida sean enteros\
+        revisa los espacios"
+    ),
+    
     (
-        ["1","c"],
-        ["Ingresa el total de diagnosticos: ", "Ingresa diagnostico: ","Casos Covid-19 = 1"],
-        "Revisa que hayas inicializado los contadores antes del ciclo, revisa si verificaste si se ingresaron valores de ese diganostico usando condicionales"
-
+        ["B","L","10"],
+        ["Tipo de cursos: ", "Tipo de alumno: ", "Cantidad de cursos: ",
+         "Colegiatura $89000 DLS", "Descuento $26700 DLS", "Total a pagar $62300 DLS"],
+        "Revisa que ingresastelos tipos de datos correctos, revisa que los datos de salida sean enteros\
+        revisa los espacios"
+    ),
+    
+    (
+        ["I","L","10"],
+        ["Tipo de cursos: ", "Tipo de alumno: ", "Cantidad de cursos: ",
+         "Colegiatura $305500 DLS", "Descuento $91650 DLS", "Total a pagar $213850 DLS"],
+         "Revisa que ingresastelos tipos de datos correctos, revisa que los datos de salida sean enteros\
+        revisa los espacios"
+    ),
+    
+      (
+        ["buenisimo","buenisimo"],
+        ["Tipo de cursos: ", "Tipo de alumno: ",  "Datos incorrectos"],
+         "No debes leer la cantidad de cursos, \
+         Revisa que ingresaste los tipos de datos correctos, revisa que los datos de salida sean enteros\
+        revisa los espacios"
+    ),
+    
+      (
+       ["cocina","chef"],
+        ["Tipo de cursos: ", "Tipo de alumno: ",  "Datos incorrectos"],
+         "No debes leer la cantidad de cursos, \
+         Revisa que ingresaste los tipos de datos correctos, revisa que los datos de salida sean enteros\
+        revisa los espacios"
+    ),
+       (
+       ["I","L","-10"],
+        ["Tipo de cursos: ", "Tipo de alumno: ",  "Cantidad de cursos: ","Cantidad de cursos incorrecta"],
+         "La cantidad de cursos debe ser mayor a 0, \
+         Revisa que ingresaste los tipos de datos correctos, revisa que los datos de salida sean enteros\
+        revisa los espacios"
+    ),
+    
+     (
+       ["I","L","0"],
+        ["Tipo de cursos: ", "Tipo de alumno: ",  "Cantidad de cursos: ","Cantidad de cursos incorrecta"],
+         "La cantidad de cursos debe ser mayor a 0, \
+         Revisa que ingresaste los tipos de datos correctos, revisa que los datos de salida sean enteros\
+        revisa los espacios"
     )
+    
+    
 ]
