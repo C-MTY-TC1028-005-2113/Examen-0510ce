28f0729803218374934608de20f854427be8b8fa
roque63 <roque@itesm.mx>
2024-03-13 22:34:54 -0600

    Update

--- assignments/02SumaNumeros/src/exercise.py
+++ assignments/02SumaNumeros/src/exercise.py
@@ -1,10 +1,18 @@
+# NO BORRAR
 # Nombre
 # Matricula
 # Fecha
 # Carrera
+#Yo________________________________________________________, me comprometo a que en 
+#la solución del examen solo usaré y aplicaré instrucciones vistas en el curso de lo 
+#contrario pido se me asigne un cero de calificación en el examen y se envié un FIA 
+#al comité de Integridad Académica, mi actuación será regida por 
+#la honestidad académica y la responsabilidad y me comprometo a
+#llevar a cabo mi examen en forma individual.
+
 
 def calcula_colegiatura_con_descuento(tipo_curso, cantidad_cursos, tipo_alumno):
-    # función que calcula el total a pagar en base a los datos recibidos en los parámetros
+    # función que calcula el total a pagar en base a los datos recibidos en los parámetros, NO MODIFICAR LOS PARÁMETROS
 
     # La función retorna el total a pagar ya con el descuento aplicado(solo si aplica)
     pass
