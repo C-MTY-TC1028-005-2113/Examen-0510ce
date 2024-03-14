4a42fdd11f11f34223febd2971ec146cdda5b7c1
roque63 <roque@itesm.mx>
2024-03-13 22:33:56 -0600

    Update exercise.py

--- assignments/01CuentaNumeros/src/exercise.py
+++ assignments/01CuentaNumeros/src/exercise.py
@@ -1,7 +1,14 @@
+# NO BORRAR
 # Nombre
 # Matricula
 # Fecha
 # Carrera
+#Yo________________________________________________________, me comprometo a que en 
+#la solución del examen solo usaré y aplicaré instrucciones vistas en el curso de lo 
+#contrario pido se me asigne un cero de calificación en el examen y se envié un FIA 
+#al comité de Integridad Académica, mi actuación será regida por 
+#la honestidad académica y la responsabilidad y me comprometo a
+#llevar a cabo mi examen en forma individual.
 
 def main():
   # Escribe tu código
